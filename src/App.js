51403f8de376ee1c2fe703d4f9750d676c469776
Seth-Trimberger <106629997+Seth-Trimberger@users.noreply.github.com>
2025-01-21 23:51:35 -0800

    Class Added

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+// Image: "CoolPicture",
+//   Name: "Jack",
+//   Address: "Test",
+//   City: "Test1",
+//   State: "Test3",
+//   ZipCode: "Test4",
+//   Category: "Test5",
+//   Rating: "Test6",
+//   ReviewCount: "Test7"
+
+const Business = {
+  return(
+
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+    
+
+    </div>
+  );
+}
+
+export default App;
